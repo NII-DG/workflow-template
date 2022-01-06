5dffd84523bd04dddb03df3d62457bf3d89c4d87
ivis-mizuguchi <rino.mizuguchi@ivis.co.jp>
2022-01-06 08:17:06 +0000

    load_json(PATH)にメソッドを修正

--- WORKFLOW/SECTIONS/base/util/load.py
+++ WORKFLOW/SECTIONS/base/util/load.py
@@ -1,12 +1,7 @@
 import json
 
-DMP = ""
-def get_dmp():
-    with open('../../../dmp.json') as f:
-        DMP = json.load(f)
-        return DMP
-
-def get_dmp_from_outsystem(DMP_PATH):
-    with open(DMP_PATH) as f:
-        DMP = json.load(f)
-        return DMP
+JSON = ""
+def load_json(PATH):
+    with open(PATH) as f:
+        JSON = json.load(f)
+        return JSON

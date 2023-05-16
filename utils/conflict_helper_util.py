a4e1e450d15302c13a4b9a20b2167fd48f621be3
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-05-16 09:14:59 +0900

    remove debug log

--- utils/conflict_helper_util.py
+++ utils/conflict_helper_util.py
@@ -1,7 +1,9 @@
 import os
 from .git import git_module
 import shutil
-from ..except_class import rename_err, action_err
+import sys
+sys.path.append('../')
+from except_class import rename_err, action_err
 
 # annex conflict options
 HEAD_REMAIN = 'HEADのファイルを残す'
@@ -46,19 +48,14 @@ def verify_resolve_file_name(file_name:str)->bool:
 
 def rename_file(base_filepath, future_name)->str:
     os.chdir(os.environ['HOME'])
-    print(f'base_filepath : {base_filepath}')
-    print(f'future_name : {future_name}')
     dirname = os.path.dirname(base_filepath)
-    print(f'dirname : {dirname}')
     future_name_filepath = f'{dirname}/{future_name}'
-    print(f'future_name_filepath : {future_name_filepath}')
 
     # os.rename(base_filepath, future_name_filepath)
     git_module.git_mv(base_filepath, future_name_filepath)
     return future_name_filepath
 
 def delete_file(file_path):
-    print(f'delete file_path : {file_path}')
     os.chdir(os.environ['HOME'])
     os.remove(file_path)
 
@@ -66,21 +63,18 @@ def copy_local_content_to_tmp(target_path:str):
     tmp_dir = get_TMP_CONFLICT_DIR()
     hash = git_module.get_local_object_hash_by_path(target_path)
     to_copy_file_path = '{}/{}'.format(tmp_dir, target_path)
-    print('to_copy_file_path : {}'.format(to_copy_file_path))
     os.chdir(os.environ['HOME'])
     make_dir(os.path.dirname(to_copy_file_path))
     os.system('git cat-file -p {} > {}'.format(hash, to_copy_file_path))
 
 def make_dir(target_dir:str):
     os.chdir(os.environ['HOME'])
-    print('make dir : {}'.format(target_dir))
     os.makedirs(target_dir, exist_ok=True)
 
 def copy_tmp_to_working(target_path:str):
     os.chdir(os.environ['HOME'])
     tmp_dir = get_TMP_CONFLICT_DIR()
     src_path = '{}/{}'.format(tmp_dir, target_path)
-    print('src_path : {}'.format(src_path))
     shutil.copy2(src_path, target_path)
 
 def copy_and_delete_tmpdir(target_paths:list[str]):

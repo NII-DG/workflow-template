6fa09e322162b12473eaba990ba1ffef88b343de
Kaho Kondo <kaho.kondo@ivis.co.jp>
2023-06-07 10:11:35 +0900

    Unify character output methods

--- utils/display_util.py
+++ utils/display_util.py
@@ -6,7 +6,7 @@ from IPython.display import display, HTML
 
 
 def creat_html_msg(msg='', fore=None, back=None, tag='h1'):
-    """HTMLを生成して表示するメソッド
+    """HTMLを生成するメソッド
     ARG
     ---------------
     msg : str
@@ -36,8 +36,9 @@ def creat_html_msg(msg='', fore=None, back=None, tag='h1'):
     else:
         return "<" + tag + " style='" + style + "'>" + msg + "</" + tag + ">"
 
-def display_msg(msg='', fore=None, back=None, tag='h1'):
-    """HTMLを生成して表示するメソッド
+
+def display_html_msg(msg='', fore=None, back=None, tag='h1'):
+    """メッセージ出力メソッド
     ARG
     ---------------
     msg : str
@@ -53,17 +54,46 @@ def display_msg(msg='', fore=None, back=None, tag='h1'):
         Description : HTMLタグ
         Default : 'h1'
     """
-    html_text = creat_html_msg(msg, None, back, default_tag)
+    html_text = creat_html_msg(msg, fore, back, tag)
     display(HTML(html_text))
 
 
+def display_log(msg='', tag='p'):
+    """赤文字でメッセージを出力するメソッド(文字色(#ff0000))
+    ARG
+    ---------------
+    msg : str
+        Description : メッセージ文字列
+        Default : ''
+    tag : str
+        Description : HTMLタグ
+        Default : 'p'
+    """
+    fore = "#ff0000"
+    display_html_msg(msg, fore, None, tag)
+
+
 default_tag = "p"
-"""メソッド : display_info()、display_err()、display_warm()のデフォルトのHTMLタグ種
+"""メソッド : display_msg()、display_info()、display_err()、display_warm()のデフォルトのHTMLタグ種
 """
 
 
+def display_msg(msg='', back=None):
+    """標準メッセージ出力メソッド(pタグ)
+    ARG
+    ---------------
+    msg : str
+        Description : メッセージ文字列
+        Default : ''
+    back : str
+        Description : 背景色
+        Default : None
+    """
+    display_html_msg(msg, None, back, default_tag)
+
+
 def display_info(msg=''):
-    """正常メッセージ出力メソッド(h2タグの背景色(#9eff9e))
+    """正常メッセージ出力メソッド(pタグの背景色(#9eff9e))
     ARG
     ---------------
     msg : str
@@ -71,12 +101,11 @@ def display_info(msg=''):
         Default : ''
     """
     back = "#9eff9e"
-    html_text = creat_html_msg(msg, None, back, default_tag)
-    display(HTML(html_text))
+    display_html_msg(msg, None, back, default_tag)
 
 
 def display_err(msg=''):
-    """異常メッセージ出力メソッド(h2タグの背景色(#ffa8a8))
+    """異常メッセージ出力メソッド(pタグの背景色(#ffa8a8))
     ARG
     ---------------
     msg : str
@@ -84,12 +113,11 @@ def display_err(msg=''):
         Default : ''
     """
     back = "#ffa8a8"
-    html_text = creat_html_msg(msg, None, back, default_tag)
-    display(HTML(html_text))
+    display_html_msg(msg, None, back, default_tag)
 
 
 def display_warm(msg=''):
-    """警告メッセージ出力メソッド(h2タグの背景色(#ffff93))
+    """警告メッセージ出力メソッド(pタグの背景色(#ffff93))
     ARG
     ---------------
     msg : str
@@ -97,5 +125,4 @@ def display_warm(msg=''):
         Default : ''
     """
     back = "#ffff93"
-    html_text = creat_html_msg(msg, None, back, default_tag)
-    display(HTML(html_text))
+    display_html_msg(msg, None, back, default_tag)

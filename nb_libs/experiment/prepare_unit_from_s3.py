73549dc50db67d5ec285d79337c058efe8113b85
ivis-futagami <ryota.futagami@ivis.co.jp>
2023-07-19 15:28:41 +0900

    add message

--- nb_libs/experiment/prepare_unit_from_s3.py
+++ nb_libs/experiment/prepare_unit_from_s3.py
@@ -2,6 +2,7 @@ import os, json, requests, urllib, csv
 from ipywidgets import Text, Button, Layout
 from IPython.display import display
 import nb_libs.utils.path.path
+import nb_libs.utils.message as mess
 
 # メソッド名変える
 def aaa():
@@ -17,19 +18,19 @@ def aaa():
         err_msg = ""
         
         if len(input_url)<=0:
-            err_msg = "オブジェクトURLが空です。修正後、再度クリックしてください。"
+            err_msg = mess.get('from_s3', 'empty_url')
         elif len(msg := (access(input_url))) > 0:
             err_msg = msg
 
         elif not input_path.startswith('input_data/') and not input_path.startswith('source/'):
-            err_msg='`input_data/`か`source/`で始まる必要があります。修正後、再度クリックしてください。'
+            err_msg = mess.get('from_s3', 'start_with')
         elif input_path == 'input_data/' or input_path == 'source/':
-            err_msg = f"`{input_path}` 以降が入力されていません。修正後、再度クリックしてください。"
+            err_msg = input_path + mess.get('from_s3', 'after_dir')
         elif os.path.isfile("experiments/" + experiment_title + "/" + input_path):
-            err_msg = f"`{input_path}` は既に存在するファイルです。修正後、再度クリックしてください。"
+            err_msg = input_path + mess.get('from_s3', 'already_exist')
 
         elif os.path.splitext(input_path)[1] != os.path.splitext(input_url)[1]:
-            err_msg = f"`{input_path}` の拡張子が元のファイルと一致しません。修正後、再度クリックしてください。"
+            err_msg = input_path + mess.get('from_s3', 'different_url')
 
         if len(err_msg) > 0:
             button.layout=Layout(width='700px')
@@ -45,24 +46,24 @@ def aaa():
         with open(os.path.join(os.environ['HOME'], '.tmp/rf_form_data/prepare_unit_from_s3.json'), mode='w') as f:
             json.dump(data, f, indent=4)
 
-        button.description='入力を完了しました。'
+        button.description = mess.get('from_s3', 'end_input')
         button.layout=Layout(width='250px')
         button.button_style='success'
 
-    button = Button(description='入力を完了する',layout=Layout(width='250px'))
+    button = Button(description=mess.get('from_s3', 'end_input'), layout=Layout(width='250px'))
     button.on_click(on_click_callback)
     text_url.on_submit(on_click_callback)
     display(text_url, text_path, button)
 
 style = {'description_width': 'initial'}
 text_path = Text(
-    description='*格納先のファイルパス：',
+    description = mess.get('from_s3', 'file_path'),
     placeholder='Enter a file path here...',
     layout=Layout(width='700px'),
     style=style
 )
 text_url = Text(
-    description='*S3にあるデータのオブジェクトURL：',
+    description=mess.get('from_s3', 'object_url'),
     placeholder='Enter a object URL here...',
     layout=Layout(width='700px'),
     style=style
@@ -75,12 +76,11 @@ def access(url):
         if response.status_code == 200:
             pass
         elif response.status_code == 404 or response.status_code == 400:
-            msg = 'URLが間違っています。'
+            msg = mess.get('from_s3', 'wrong_url'),
         elif response.status_code == 403:
-            msg = 'プライベートなオブジェクトです。'
-
+            msg = mess.get('from_s3', 'private_url'),
     except requests.exceptions.RequestException:
-        msg = 'URLが間違っています。'
+        msg = mess.get('from_s3', 'wrong_url'),
     return msg
 
 def bbb():

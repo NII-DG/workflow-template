fc75388d5b8f339cdc01b8a2c2cf0e052d7e64d3
Kaho Kondo <kaho.kondo@ivis.co.jp>
2023-07-20 16:16:59 +0900

    modify link method

--- nb_libs/utils/path/display.py
+++ nb_libs/utils/path/display.py
@@ -1,19 +1,47 @@
 import os
-from ..message import message, display
+from ..message import message
 from . import path
+
+
+def move_button(url:str, msg:str):
+    """同じページで画面遷移するボタンを表示する
+
+    ARG
+    ---------------
+    url : str
+        Description : リンク先のURL
+    msg : str
+        Description : 表示する説明
+    """
+    html_text = f'<button onclick="location.herf=\'{url}\'">{msg}</button>'
+    return display(HTML(html_text))
+
+
+def link_button(url:str, msg:str)
+    """別タブでページを開くボタンを表示する
+
+    ARG
+    ---------------
+    url : str
+        Description : リンク先のURL
+    msg : str
+        Description : 表示する説明
+    """
+    html_text = f'<button onclick="window.open(\'{url}\')">{msg}</button>'
+    return display(HTML(html_text))
 
 
 def res_top_link():
     """研究フロートップページへのリンクを表示する"""
-    display.desplay_link('./base_FLOW.ipynb', message.get("transition", "reserch_top"))
+    move_button('./base_FLOW.ipynb', message.get("transition", "reserch_top"))
 
 
 def res_top_link_from_maDMP():
     """研究フロートップページへのリンクを表示する"""
     top_path = os.path.join('./', path.FLOW_DIR, path.NOTEBOOK_DIR, path.RESEARCH_DIR, 'base_FLOW.ipynb')
-    display.desplay_link(top_path, message.get("transition", "reserch_top"))
+    move_button(top_path, message.get("transition", "reserch_top"))
 
 
 def exp_top_link():
     """実験フロートップページへのリンクを表示する"""
-    display.desplay_link('./experimnet.ipynb', message.get("transition", "experiment_top"))
+    move_button('./experimnet.ipynb', message.get("transition", "experiment_top"))

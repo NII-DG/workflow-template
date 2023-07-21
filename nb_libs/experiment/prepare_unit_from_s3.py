e62d8fbd63d6b600f26d179d3a45e5d249878ed7
ivis-futagami <ryota.futagami@ivis.co.jp>
2023-07-21 09:38:05 +0900

    fix validation

--- nb_libs/experiment/prepare_unit_from_s3.py
+++ nb_libs/experiment/prepare_unit_from_s3.py
@@ -95,7 +95,7 @@ def validate_url(url) -> str:
         elif response.status_code == 404 or response.status_code == 400:
             msg = mess.get('from_s3', 'wrong_url')
         elif response.status_code == 403:
-            msg = mess.get('from_s3', 'private_url')
+            msg = mess.get('from_s3', 'private_object')
         else:
             msg = mess.get('from_s3', 'exception')
     except requests.exceptions.RequestException:

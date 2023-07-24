9ff7ccb62ce1987fc0cb2a95ff5ae86cb455a1e6
Kaho Kondo <kaho.kondo@ivis.co.jp>
2023-07-24 13:39:06 +0900

    fix

--- nb_libs/utils/path/display.py
+++ nb_libs/utils/path/display.py
@@ -19,7 +19,7 @@ def button_html(url:str, msg:str, target='_self'):
 def res_top_html():
     """研究フロートップページへのリンクのhtmlを生成する"""
     top_path = os.path.join('./', path.RESEARCH_TOP)
-    return button_html(top_path, message.get("menu", "reserch_top"))
+    return button_html(top_path, message.get("menu", "trans_reserch_top"))
 
 
 def res_top_link():
@@ -31,14 +31,14 @@ def res_top_link():
 def res_top_link_from_maDMP():
     """研究フロートップページへのリンクを表示する"""
     top_path = os.path.join('./', path.FLOW_DIR, path.NOTEBOOK_DIR, path.RESEARCH_DIR, path.RESEARCH_TOP)
-    html_text = button_html(top_path, message.get("menu", "reserch_top"))
+    html_text = button_html(top_path, message.get("menu", "trans_reserch_top"))
     display(HTML(html_text))
 
 
 def exp_top_html():
     """実験フロートップページへのリンクのhtmlを生成する"""
     top_path = os.path.join('./', path.EXPERIMENT_TOP)
-    return button_html(top_path, message.get("menu", "experiment_top"))
+    return button_html(top_path, message.get("menu", "trans_experiment_top"))
 
 
 def exp_top_link():

84211d68d2b7dece0eaf865270c186c4330dae22
ivis-futagami <ryota.futagami@ivis.co.jp>
2023-07-26 18:44:56 +0900

    add aws connect info

--- nb_libs/experiment/prepare_multi_from_s3.py
+++ nb_libs/experiment/prepare_multi_from_s3.py
@@ -1,7 +1,7 @@
 '''prepare_multi_from_s3.ipynbから呼び出されるモジュール'''
-import os, json, urllib, traceback
+import os, json, urllib, traceback, boto3
 from json.decoder import JSONDecodeError
-from ipywidgets import Text, Button, Layout
+from ipywidgets import Text, Button, Layout, Password
 from IPython.display import display, clear_output, Javascript
 import panel as pn
 pn.extension()
@@ -18,10 +18,16 @@ from ..utils.except_class import DidNotFinishError, UnexpectedError
 # 辞書のキー
 PATH_TO_URL = 'path_to_url'
 EX_PKG_NAME = 'ex_pkg_name'
+
 AWS_S3_INFO = 'aws_s3_info'
+AWS_REGION_CODE = 'aws_region_code'
+BUCKET = 'bucket'
+PREFIX = 'prefix'
 PATHS = 'paths'
 
-
+LOCATION_CONSTRAINT = 'LocationConstraint'
+CONTENTS = 'Contents'
+KEY = 'Key'
 
 def get_experiment_title() -> str:
     '''ex_pkg_info.jsonを開いて実験パッケージ名を取得する
@@ -65,21 +71,60 @@ def get_path_to_url_dict() -> dict:
         display_util.display_err(message.get('from_repo_s3', 'unexpected'))
         raise
 
+# あとで消す
+# ここまで共通関数
 #############################################################################################################################
-
 
 def input_url_path():
 
-
-    from ipywidgets import Text, Button, Layout, Password
-
-    
-
     def on_click_callback(clicked_button: Button) -> None:
+
+        common.delete_file(path.MULTI_S3_JSON_PATH)
+
+        bucket_name = input_bucket_name.value
+        prefix = input_prefix.value
+
+        s3 = boto3.resource(
+            's3',
+            aws_access_key_id = input_aws_access_key_id.value,
+            aws_secret_access_key = input_aws_secret_access_key.value,
+        )
+        bucket = s3.Bucket(bucket_name)
+
+        response = bucket.meta.client.get_bucket_location(Bucket=bucket.name)
+        aws_region = response[LOCATION_CONSTRAINT]
+        
+        
+        if len(prefix)==0:
+            response = bucket.meta.client.list_objects_v2(Bucket=bucket.name)
+        else:
+            response = bucket.meta.client.list_objects_v2(Bucket=bucket.name, Prefix=prefix)
+
+        s3_contens_key_list = []
+        if not CONTENTS in response:
+            pass
+        else:
+            for content in response[CONTENTS]:
+                if not content[KEY].endswith('/'):
+                    s3_contens_key_list.append(content[KEY])
+
+        aws_s3_info_dict = dict()
+        data = dict()
+        data[AWS_REGION_CODE] = aws_region
+        data[BUCKET] = bucket_name
+        data[PREFIX] = prefix
+        data[PATHS] = s3_contens_key_list
+        aws_s3_info_dict[AWS_S3_INFO] = data
+
+        with open(path.MULTI_S3_JSON_PATH, mode='w') as f:
+            json.dump(aws_s3_info_dict, f, indent=4)
+
+
         button.description='入力を受け付けました。'
         button.button_style='success'
 
     # テキストボックス
+    style = {'description_width': 'initial'}
     input_aws_access_key_id = Password(
         description='*AWSアクセスキーID：',
         placeholder='Enter your AWS access key ID here...',
@@ -97,22 +142,21 @@ def input_url_path():
         description='*バケット名：',
         placeholder='Enter S3 bucket name here...',
         layout=Layout(width='700px'),
-        style=style
+        style=style,
+        value='test-data-rf'
     )
     input_prefix = Text(
         description='バケットの任意のフォルダパス：',
         placeholder='Enter bucket folder path here...',
         layout=Layout(width='700px'),
-        style=style
+        style=style,
+        value='pick_folder/'
     )
 
-    style = {'description_width': 'initial'}
+    common.delete_file(path.MULTI_S3_JSON_PATH)
     button = Button(description='入力を完了する', layout=Layout(width='200px'))
     button.on_click(on_click_callback)
     display(input_aws_access_key_id, input_aws_secret_access_key, input_bucket_name, input_prefix, button)
-
-
-
 
 
 

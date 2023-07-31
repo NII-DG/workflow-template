e226c29ee9bd6e5636cf67fdc588b13b833673cd
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-07-31 18:20:58 +0900

    create temp

--- nb_libs/common/conflict_helper.py
+++ nb_libs/common/conflict_helper.py
@@ -4,6 +4,66 @@ from ..utils.path import path, display as pd
 from ..utils.message import message
 from IPython.display import HTML, display
 
+
+
+def analyze_conflict_status():
+    """1. 競合状態の解析
+    """
+    pass
+
+def get_annex_variatns():
+    """2-1. annex競合バリアントファイルの入手
+    """
+    pass
+
+def record_preparing_event_for_resolving_conflict():
+    """2-2. 競合解消準備をリポジトリ履歴に記録
+    """
+    pass
+
+def resolving_git_content():
+    """3-1. gitコンテンツの競合解消
+    """
+    pass
+
+def select_action_for_resolving_annex():
+    """3-2. Annexコンテンツの競合解消アクションを選択
+    """
+    pass
+
+def rename_variants():
+    """3-3. ≪両方を残す≫を選択したファイル名の入力
+    """
+    pass
+
+def auto_resolve_task_notebooks():
+    """4-1. データの調整 - タスクNotebookの自動解消
+    """
+    pass
+
+def adjust_annex_data():
+    """4-1. データの調整 - Annexコンテンツのバリアントファイルのデータ調整
+    """
+    pass
+
+def prepare_sync():
+    """4-2. 同期の準備
+    """
+    pass
+
+
+RF_FORM_FILE = os.path.join(path.RF_FORM_DATA_DIR, 'conflict_helper.json')
+
+
+def exist_rf_form_file()->bool:
+    """Check for the existence of the conflict_helper.json file
+
+    Returns:
+        [bool]: [True : exist, False : no exist]
+    """
+    return os.path.exists(RF_FORM_FILE)
+
+
 def trans_top():
     """Display a link button to the Study or Execution Flow Top Page.
 

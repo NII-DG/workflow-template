bda5795d5df5fcfc1103db941e4f6157a2685212
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-07-31 11:05:33 +0900

    up launch_ex_env

--- nb_libs/research/base_launch_an_experiment.py
+++ nb_libs/research/base_launch_an_experiment.py
@@ -1,7 +1,27 @@
+import requests
+from ..utils.gin import sync
+from ..utils.except_class import RepositoryNotExist, UrlUpdateError
+from ..utils.message import display as msg_display, message
 
 
 def launch_ex_env():
     # Up-to-date repository information in containers
+    try:
+        is_praivate = sync.update_repo_url()
+    except requests.exceptions.RequestException as e:
+        # Poor connection to GIN-fork
+        err_msg_with_reason = message.get('launch', 'fail_update_repo_info')
+        reason = message.get('DEFAULT', 'fail_connnection_for_gin')
+        msg_display.display_err(err_msg_with_reason.format(reason))
+        msg_display.display_log(msg=str(e))
+        return
+    except RepositoryNotExist as e:
+        # Repository unidentifiable
+        pass
+    except UrlUpdateError as e:
+        # Unexpected errors
+        pass
+
     # Check whether the repository is private or public.
     is_praivate = True
 

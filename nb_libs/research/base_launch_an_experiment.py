082af44acf7242a7328ad61498a2aba9403c962b
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-07-31 10:43:05 +0900

    launchタスク作成

---
+++ nb_libs/research/base_launch_an_experiment.py
@@ -0,0 +1,23 @@
+
+
+def launch_ex_env():
+    # Up-to-date repository information in containers
+    # Check whether the repository is private or public.
+    is_praivate = True
+
+    if is_praivate:
+        # It's private.
+        ## Create launchURL for private repository.
+        ### Obtain a GIN-fork token.
+        ### Obtain a token for construction from GIN-fork.
+        ### User information is obtained from GIN-fork.
+        ### Create launchURL for private repository.
+
+        pass
+    else:
+        # It's public.
+
+        ## Create launchURL for public repository.
+        pass
+
+    # Display the Create Experiment Run Environment button.

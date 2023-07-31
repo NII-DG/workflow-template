e58665e448baeada8056af883909f35ff28ca7ce
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-07-31 17:41:51 +0900

    add top link for conflict

--- nb_libs/common/conflict_helper.py
+++ nb_libs/common/conflict_helper.py
@@ -1,3 +1,8 @@
+import os
+from ..utils.params import ex_pkg_info as epi
+from ..utils.path import path, display as pd
+from ..utils.message import message
+from IPython.display import display, HTML
 
 def trans_top():
     """Display a link button to the Study or Execution Flow Top Page.
@@ -8,3 +13,15 @@ def trans_top():
 
     # Identify whether the function execution environment is research or experimental
     # If the ex_pkg_info.json file is not present, the research execution environment, and if present, the experimental execution environment.
+
+    html_text = ''
+    if epi.exist_file():
+        # For experiment execution environment
+        top_path = os.path.join('./../', path.RESEARCH_DIR ,path.RESEARCH_TOP)
+        html_text = pd.button_html(top_path, message.get("menu", "trans_reserch_top"))
+    else:
+        # For research execution environment
+        top_path = os.path.join('./../', path.EXPERIMENT_DIR ,path.EXPERIMENT_TOP)
+        html_text = pd.button_html(top_path, message.get("menu", "trans_reserch_top"))
+
+    display(HTML(html_text))
